281674471c8e9e5a643ded232c59568dfed72108
DreamerYu <hongyu0831@gmail.com>
2018-05-13 23:08:36 +0800

    problems

---
+++ 209-Minimum Size Subarray Sum.java
@@ -0,0 +1,43 @@
+class Solution {
+    //O(n)
+    public int minSubArrayLen(int s, int[] nums) {
+        int n = nums.length;
+        int ans = Integer.MAX_VALUE;
+        int left = 0;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            while (sum >= s) {
+                ans = Math.min(ans, i + 1 - left);
+                sum -= nums[left++];
+            }
+        }
+        return (ans != Integer.MAX_VALUE) ? ans : 0;
+        
+    }
+    
+    //binary search
+    public int minSubArrayLen2(int s, int[] nums) {
+        int[] sums = new int[nums.length + 1];
+        for (int i = 1; i < sums.length; i++) sums[i] = sums[i - 1] + nums[i - 1];
+        int minLen = Integer.MAX_VALUE;
+        for (int i = 0; i < sums.length; i++) {
+            int end = binarySearch(i + 1, sums.length - 1, sums[i] + s, sums);
+            if (end == sums.length) break;
+            if (end - i < minLen) minLen = end - i;
+        }
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
+    
+    private int binarySearch(int lo, int hi, int key, int[] sums) {
+        while (lo <= hi) {
+           int mid = (lo + hi) / 2;
+           if (sums[mid] >= key){
+               hi = mid - 1;
+           } else {
+               lo = mid + 1;
+           }
+        }
+        return lo;
+    }
+}
